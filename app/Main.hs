53caefc1410d8f1590669cb7048787611b72018b
Nikhil Siwach <nikhilsiwach002@gmail.com>
2023-12-28 23:46:47 +0530

    Comment the Example main

--- app/Main.hs
+++ app/Main.hs
@@ -5,14 +5,14 @@ import Network.Wai.Handler.Warp (run)
 import ParserService  -- internal Imports
 import Server
 
-main :: IO ()   --Example function without API
-main = do
-  let encodedString = "#00date|1997-02-06#02name|bob#01age|20#03hasPassport|Y#13access|y,n,F,f"
-  let maybeJson = parseToJSON encodedString
-  case maybeJson of
-    Just json -> do 
-      print (encode (toJSON json))
-    Nothing -> putStrLn "Invalid encoding format"
+-- main :: IO ()   --Example function without API
+-- main = do
+--   let encodedString = "#00date|1997-02-06#02name|bob#01age|20#03hasPassport|Y#12access|access|read_db,write_db,view_logs"
+--   let maybeJson = parseToJSON encodedString
+--   case maybeJson of
+--     Just json -> do 
+--       print (encode (toJSON json))
+--     Nothing -> putStrLn "Invalid encoding format"
     
 
 
--- src/Server.hs
+++ src/Server.hs
@@ -1,30 +1,30 @@
 module Server where
 
 
--- import Data.HashMap.Strict (HashMap)
--- import Data.Text (Text)
+import Data.HashMap.Strict (HashMap)
+import Data.Text (Text)
 
--- import ErrorHandler
--- import ParserService
-
-
+import ErrorHandler
+import ParserService
 
 
--- type MyAPI = "parse" :> QueryParam "data" String :> Get '[JSON] (Either (Int,Text) (HashMap Text Value))
-
--- parseData :: Maybe String -> Handler (Either Text (HashMap Text Value))
--- parseData maybeEncodedString = case maybeEncodedString of
---   Just encodedString -> case parseToJSON encodedString of
---     Just json -> return $ Right json
---     Nothing -> throwError $ toError InternalServerError
---   Nothing -> throwError $ toError InvalidDataError
---   where
---     toError err = T.pack . show . toHttpStatusCode $ err
 
 
--- server :: Server MyAPI
--- server = parseData
+type MyAPI = "parse" :> QueryParam "data" String :> Get '[JSON] (Either (Int,Text) (HashMap Text Value))
 
--- app :: Application
--- app = serve (Proxy :: Proxy MyAPI) server
+parseData :: Maybe String -> Handler (Either Text (HashMap Text Value))
+parseData maybeEncodedString = case maybeEncodedString of
+  Just encodedString -> case parseToJSON encodedString of
+    Just json -> return $ Right json
+    Nothing -> throwError $ toError InternalServerError
+  Nothing -> throwError $ toError InvalidDataError
+  where
+    toError err = T.pack . show . toHttpStatusCode $ err
+
+
+server :: Server MyAPI
+server = parseData
+
+app :: Application
+app = serve (Proxy :: Proxy MyAPI) server
 
